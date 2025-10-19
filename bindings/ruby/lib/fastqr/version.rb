f784f79c0bf25203498a4d8f5251a26c1a1a57b3
Dora <tranhuucanh39@gmail.com>
2025-10-20 00:19:01 +0700

    chore: bump version to 1.0.21

--- bindings/ruby/lib/fastqr/version.rb
+++ bindings/ruby/lib/fastqr/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module FastQR
-  VERSION = "1.0.20"
+  VERSION = "1.0.21"
 end
 
--- src/fastqr.cpp
+++ src/fastqr.cpp
@@ -25,7 +25,7 @@
 // Enable benchmarking
 // #define FASTQR_BENCHMARK
 
-#define FASTQR_VERSION "1.0.20"
+#define FASTQR_VERSION "1.0.21"
 
 namespace fastqr {
 
