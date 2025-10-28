d533ddfefa6cdf17a524c9a387a29f0a4c274c3b
Dora <tranhuucanh39@gmail.com>
2025-10-28 15:41:38 +0700

    Update license

--- bindings/nodejs/lib/platform.js
+++ bindings/nodejs/lib/platform.js
@@ -1,3 +1,23 @@
+/*
+ * FastQR - Fast QR Code Generator Library
+ * Copyright (C) 2025 Tran Huu Canh and FastQR Contributors
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, see <https://www.gnu.org/licenses/>.
+ *
+ * Homepage: https://github.com/tranhuucanh/fastqr
+ */
+
 const os = require('os');
 const path = require('path');
 const fs = require('fs');
